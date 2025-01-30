6d31a1fa26e399be9f25adfef3ce26dac12bb50c
sumitnagrikar <sumitnagrikar1@gmail.com>
2025-01-30 18:34:23 +0530

    add create and getuser controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,19 @@
+import httpStatus from 'http-status';
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+import { userService } from '../services';
+
+const createUser = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+  res.status(httpStatus.CREATED).send(user);
+});
+
+const getUser = catchAsync(async (req, res) => {
+  const user = await userService.getUserById(req.params.userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  res.send(user);
+});
+
+export { createUser, getUser };
