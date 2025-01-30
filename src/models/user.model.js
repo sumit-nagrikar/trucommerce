01e789129fac7b9cd69cf352107c1cbd33d8028c
sumitnagrikar <sumitnagrikar1@gmail.com>
2025-01-30 15:00:48 +0530

    add validation to email, pass and add methods is emailtaken, match password and used pre hook to hash password when modified

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,22 +1,79 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
+import { toJSON } from './plugins/toJSON.plugin';
+import { paginate } from './plugins/paginate.plugin';
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Invalid email');
+        }
+      },
+    },
+    validate(value) {
+      if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+        throw new Error(
+          'Password must contain at least one letter and one number'
+        );
+      }
+    },
     role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
     address: { type: mongoose.Schema.Types.ObjectId, ref: 'Address' }, // Reference to Address model
     cart: [
       {
         product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-        quantity: { type: Number, default: 1 },
+        quantity: {
+          type: Number,
+          default: 1,
+          min: [1, 'Quantity cannot be less than 1'],
+        },
       },
     ], // Array of products in cart
   },
   { timestamps: true, minimize: false }
 );
 
-const userModel = mongoose.models.User || mongoose.model('User', userSchema);
+// add plugin that converts mongoose to json
+userSchema.plugin(toJSON);
+userSchema.plugin(paginate);
 
-export default userModel;
+/**
+ * Check if email is taken
+ * @param {string} email - The user's email
+ * @param {ObjectId} [excludeUserId] - The id of the user to be excluded
+ * @returns {Promise<boolean>}
+ */
+userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
+  const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
+  return !!user;
+};
+
+/**
+ * Check if password matches the user's password
+ * @param {string} password
+ * @returns {Promise<boolean>}
+ */
+userSchema.methods.isPasswordMatch = async function (password) {
+  const user = this;
+  return bcrypt.compare(password, user.password);
+};
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const user = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default user;
