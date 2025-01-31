5c6aa805c5b28e0392bb03cae19ba1981974b341
sumitnagrikar <sumitnagrikar1@gmail.com>
2025-01-31 18:51:18 +0530

    add main route and app.use middlewares

--- src/app.js
+++ src/app.js
@@ -1,20 +1,67 @@
-import express from 'express';
-import userRoutes from './routes/v1/user.routes';
-import cors from 'cors';
+const express = require('express');
+const helmet = require('helmet');
+// const xss = require('xss');
+const mongoSanitize = require('express-mongo-sanitize');
+const compression = require('compression');
+const cors = require('cors');
+const passport = require('passport-jwt');
+const httpStatus = require('http-status');
+const config = require('./config/config');
+const morgan = require('./config/morgan');
+const { jwtStrategy } = require('./config/passport');
+const { authLimiter } = require('./middlewares/rateLimiter');
+const routes = require('./routes/v1');
+const { errorConverter, errorHandler } = require('./middlewares/error');
+const ApiError = require('./utils/ApiError');
 
 const app = express();
 
-//Middleware
-app.use(cors());
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to Trucommerce Backend');
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+// app.use(xss());
+app.use(mongoSanitize());
+
+// gzip compression
+app.use(compression()); //
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+// jwt authentication
+// app.use(passport.initialize());
+// passport.use('jwt', jwtStrategy);
+
+// limit repeated failed requests to auth endpoints
+if (config.env === 'production') {
+  app.use('/v1/auth', authLimiter);
+}
+
+// v1 api routes
+app.use('/v1', routes);
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
-// User routes
+// convert error to ApiError, if needed
+app.use(errorConverter);
 
-app.use('/api/v1/users', userRoutes);
+// handle error
+app.use(errorHandler);
 
-export default app;
+module.exports = app;
