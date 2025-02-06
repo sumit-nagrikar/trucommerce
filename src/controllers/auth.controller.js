a1646bf7ca9702bfad04d71f673a55ebfee99c01
Sumit Nagrikar <sumitnagrikar1@gmail.com>
2025-02-06 12:39:56 +0530

    Add admin login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,15 +7,15 @@ const {
   adminService,
 } = require('../services');
 
-//user service going to save user data with role for later RBAC
 const register = catchAsync(async (req, res) => {
-  const user = await userService.createUser(req.body); //create user object
-  const tokens = await tokenService.generateAuthTokens(user); // create token object
+  const user = await userService.createUser(req.body);
+  const tokens = await tokenService.generateAuthTokens(user);
   res
     .status(httpStatus.status.CREATED)
-    .send({ message: 'Registerd successfully', user, tokens }); //send both user and token in another object
+    .send({ message: 'Registerd successfully', user, tokens });
 });
 
+//login based on email and password
 const login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const user = await authService.loginUserWithEmailAndPassword(email, password);
@@ -23,17 +23,21 @@ const login = catchAsync(async (req, res) => {
   res.send({ message: 'Login successful', user, tokens });
 });
 
+//admin will login then access protected routes
 const admin = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const admin = await adminService.adminLogin(email, password);
   const tokens = await tokenService.generateAuthTokens(admin);
   res.status(200).send({ message: 'Admin login successful', admin, tokens });
 });
+
+//Delete refresh token hence automatic logout happens
 const logout = catchAsync(async (req, res) => {
   await authService.logout(req.body.refreshToken);
   res.status(httpStatus.status.OK).send({ message: 'Logout successful' });
 });
 
+//Delete the old token and generate a new one
 const refreshTokens = catchAsync(async (req, res) => {
   const tokens = await authService.refreshAuth(req.body.refreshToken);
   res.send({ ...tokens });
