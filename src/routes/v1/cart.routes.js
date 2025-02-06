bdca500ea8e6a65c243c61ee54a066d4101ffc04
Sumit Nagrikar <sumitnagrikar1@gmail.com>
2025-02-06 12:44:46 +0530

    add routes and auth

--- src/routes/v1/cart.routes.js
+++ src/routes/v1/cart.routes.js
@@ -2,18 +2,28 @@ const express = require('express');
 const { cartController } = require('../../controllers');
 const validate = require('../../middlewares/validate');
 const { cartValidation } = require('../../validations');
+const authenticate = require('../../middlewares/authenticate.cart');
 const router = express.Router();
 
-// Get the user's cart
-router.get('/:userId', validate(cartValidation.getCart), cartController.getCart);
+// Get the logged-in user's cart
+router.get('/', authenticate(), cartController.getCart);
 
-// Add a product to the user's cart
-router.post('/add', validate(cartValidation.addItemToCart), cartController.addToCart);
+// Add a product to the cart
+router.post(
+  '/add',
+  authenticate(),
+  validate(cartValidation.addItemToCart),
+  cartController.addToCart
+);
 
-// Update the quantity of a product in the user's cart
-router.delete('/remove/:productId', validate(cartValidation.updateItemInCart), cartController.removeFromCart);
+// Remove a product from the cart
+router.delete(
+  '/remove/:productId',
+  authenticate(),
+  cartController.removeFromCart
+);
 
-// Clear the user's cart
-router.delete('/clear', cartController.clearCart);
+// Clear the cart
+router.delete('/clear', authenticate(), cartController.clearCart);
 
 module.exports = router;
