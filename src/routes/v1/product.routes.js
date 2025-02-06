935ffc8da7d25426ce574e5d99aea00dfd501f38
Sumit Nagrikar <sumitnagrikar1@gmail.com>
2025-02-06 12:46:24 +0530

    add admin auth middleware in request

--- src/routes/v1/product.routes.js
+++ src/routes/v1/product.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const validate = require('../../middlewares/validate');
 const { productController } = require('../../controllers');
 const productValidation = require('../../validations/product.validation');
+const isAdmin = require('../../middlewares/isAdmin');
 
 const router = express.Router();
 
 // Create a new product
 router.post(
   '/',
+  isAdmin, // Only admin can create products
   validate(productValidation.createProduct),
   productController.createProduct
 );
@@ -21,11 +23,12 @@ router.get('/:id', productController.getProductById);
 // Update a product by ID
 router.patch(
   '/:id',
+  isAdmin,
   validate(productValidation.updateProduct),
   productController.updateProduct
 );
 
 // Delete a product by ID
-router.delete('/:id', productController.deleteProduct);
+router.delete('/:id', isAdmin, productController.deleteProduct);
 
 module.exports = router;
