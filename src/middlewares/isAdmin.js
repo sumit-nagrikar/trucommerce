adbeae805585d8104086826a45f016080eae7c53
Sumit Nagrikar <sumitnagrikar1@gmail.com>
2025-02-06 12:43:33 +0530

    add admin auth

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,20 +1,29 @@
+const httpStatus = require('http-status');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
+const ApiError = require('../utils/ApiError');
 
-const isAdmin = (req, res, next) => {
+const authAdmin = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1]; // get token from request header authorization array
+  if (!token) {
+    return next(
+      new ApiError(httpStatus.status.UNAUTHORIZED, 'Token Not Found')
+    );
+  }
+
   try {
-    const token = req.header('Authorization').replace('Bearer ', '');
     const decoded = jwt.verify(token, config.jwt.secret);
 
-    if (decoded.type !== 'admin') {
-      return res.status(403).json({ message: 'Access denied, admin only.' });
+    // Check if role exists and it's admin
+    if (decoded.role !== 'admin') {
+      return next(new ApiError(httpStatus.status.FORBIDDEN, 'Access denied'));
     }
 
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'Please authenticate as admin' });
+    return next(new ApiError(httpStatus.status.UNAUTHORIZED, 'Invalid token'));
   }
 };
 
-module.exports = isAdmin;
+module.exports = authAdmin;
