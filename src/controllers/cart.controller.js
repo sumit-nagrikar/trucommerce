b0171b8d2a74265e56647a4df5bceffe05322c35
Sumit Nagrikar <sumitnagrikar1@gmail.com>
2025-02-06 12:41:25 +0530

    add cart crud operations

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,29 +1,40 @@
 const catchAsync = require('../utils/catchAsync');
 const cartService = require('../services/cart.service');
-const logger = require('../config/logger');
 
 const getCart = catchAsync(async (req, res) => {
-    console.log("id:",req.user);
-    // logger.info(req.user);//id received undefined
   const cart = await cartService.getCart(req.user.id);
-  res.json(cart);
+  res.status(200).json(cart);
 });
 
 const addToCart = catchAsync(async (req, res) => {
   const { productId, quantity } = req.body;
-  const cart = await cartService.addToCart(req.user.id, productId, quantity);
-  res.json(cart);
+  const updatedCart = await cartService.addToCart(
+    req.user.id,
+    productId,
+    quantity
+  );
+  res.status(200).json(updatedCart);
 });
 
 const removeFromCart = catchAsync(async (req, res) => {
   const { productId } = req.params;
-  const cart = await cartService.removeFromCart(req.user.id, productId);
-  res.json(cart);
+  const updatedCart = await cartService.removeFromCart(req.user.id, productId);
+
+  if (!updatedCart) {
+    return res.status(404).json({ message: 'Product not found in cart' });
+  }
+
+  res.status(200).json(updatedCart);
 });
 
 const clearCart = catchAsync(async (req, res) => {
   await cartService.clearCart(req.user.id);
-  res.json({ message: 'Cart cleared' });
+  res.status(200).json({ message: 'Cart cleared successfully' });
 });
 
-module.exports = { getCart, addToCart, removeFromCart, clearCart };
+module.exports = {
+  getCart,
+  addToCart,
+  removeFromCart,
+  clearCart,
+};
